708f2c61ad62c4853e1e47bca59428b99cd949fd
Surajkumar R Ettireddy <eskreddy@beyster1695p26.engin.umich.edu>
2018-11-07 12:56:16 -0500

    added result

--- rpn.py
+++ rpn.py
@@ -30,7 +30,7 @@ def calculate(myarg):
             elif token == '*':
                 print(colored(arg1, 'green'), colored(token + str(arg2), 'green'))
             elif token == '/':
-                print(colored(arg1, 'green'), colored(token + str(arg2), 'red'))
+                print(colored(arg1, 'green'), colored(token + " " + str(arg2), 'red'), colored(" = " + str(result), 'orange'))
             stack.append(result)
         print(stack)
     if len(stack) != 1:
